f2767c9b32c5554f6e48132b9f30a334a8502815
Josiah Parry <josiah.parry@gmail.com>
2023-12-20 15:22:30 -0500

    update

--- post-render.R
+++ post-render.R
@@ -1,12 +1,16 @@
+# this should be printed to stdout
 message(Sys.getenv("QUARTO_PROJECT_OUTPUT_FILES"))
 
-library(rvest)
 
-html <- read_html("index.html")
-doc <- html_children(html)
-xml2::xml_remove(html_node(doc, "#heading-id"))
+# this should read the written html file
+html <- rvest::read_html("index.html")
+doc <- rvest::html_children(html)
+
+# remove html elemnt
+xml2::xml_remove(rvest::html_node(doc, "#heading-id"))
 
 # this should save a new html file
 htmltools::save_html(doc, "testing.html")
 
+# this should create a new file called out.txt
 writeLines("hello world", "out.txt")
